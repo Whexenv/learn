fa43d0e3efec66f748ba225013bc39c2b1c448e3
Berk-Turhan <167649224+Berk-Turhan@users.noreply.github.com>
2026-06-09 15:13:00 +0300

    edit code

--- Assets/scripts/playercontrol.cs
+++ Assets/scripts/playercontrol.cs
@@ -4,7 +4,7 @@ public class playercontrol : MonoBehaviour
 {
     public float speed = 5f;
     public float fastspeed = 50f;
-    public float slowspeed = 2f ;
+    public float slowspeed = 2f;
 
     public float currentSpeed;
     public bool control_dead;
@@ -17,76 +17,81 @@ public class playercontrol : MonoBehaviour
 
     void Start()
     {
-        currentSpeed = speed; 
+        currentSpeed = speed;
     }
 
     void Update()
+
     {
-       if (control_dead == true)
+        if (control_dead == true)
         {
-            Destroy(player);
             print("Yaşamadan nasıl yürücen Babo");
-        }
-        
-       
-        if (Input.GetKey(KeyCode.LeftShift))
-        {
+            Destroy(player);
+            return;
 
-            currentSpeed = fastspeed;
 
         }
-      
-         else if (Input.GetKey(KeyCode.LeftControl))
-        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
 
-            currentSpeed = slowspeed;
+                currentSpeed = fastspeed;
+
+            }
+
+            else if (Input.GetKey(KeyCode.LeftControl))
+            {
+
+                currentSpeed = slowspeed;
+
+            }
+            else
+            {
+                currentSpeed = speed;
+
+            }
+            Vector3 yon = Vector3.zero;
+            if (Input.GetKey(KeyCode.W))
+            {
+                yon += Vector3.forward;
+
+                //  transform.position += Vector3.left * currentSpeed * Time.deltaTime; normalde  böyle yazsak biraz daha uzun oluyor  en üstte vector 3 "yön diye 
+                //tanımladığımız değişkeni üst satırda gördüğümüz gibi  tek bir yerden gelmesi lazım  77. satırdada tanımladığımız transform.position yerinde hepsini tek bir transform position na tanımladık 
+
+
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                yon += Vector3.left;
+
+
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                yon += Vector3.back;
+
+
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+
+                yon += Vector3.right;
+
+            }
+            transform.position += yon.normalized * Time.deltaTime * currentSpeed;
+            //normalized dememizin sebebi çaprak giderken hız artyıor biraz "normalized" dediğimizde bu adı üstünde normal hale geliyor
+
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                gun.SetActive(true);
+                gun2.SetActive(false);
+
+            }
+            else if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                gun2.SetActive(true);
+                gun.SetActive(false);
+
+            }
 
         }
-        else
-        {
-            currentSpeed = speed;
-
-        }
-
-
-
-        if (Input.GetKey(KeyCode.W))
-        {
-
-            transform.position += Vector3.forward * currentSpeed*Time.deltaTime;
-        
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-
-            transform.position += Vector3.left * currentSpeed * Time.deltaTime;
-
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-
-            transform.position += Vector3.back * currentSpeed * Time.deltaTime;
-
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-
-            transform.position += Vector3.right * currentSpeed * Time.deltaTime;
-
-        }
-        if (Input.GetKeyDown(KeyCode.Alpha1))
-        {
-            gun.SetActive(true);
-            gun2.SetActive(false);
-
-        }
-        else if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            gun2.SetActive(true);
-            gun.SetActive(false);
-
-        }
-
-
     }
-}
